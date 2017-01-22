9d3a7aad6e16a66c947a6ac354c18d5e0ac28ba7
mnaylor5 <mitchnaylor94@gmail.com>
2017-01-22 16:30:31 -0500

    Fixed the png creation code

--- plot_1.R
+++ plot_1.R
@@ -5,6 +5,6 @@ consumption <- read.table('household_power_consumption.txt', header = T, sep = '
                           na.strings = '?')
 consumption$Date <- as.Date(consumption$Date, format = "%d/%m/%Y")
 consumption <- consumption[consumption$Date == '2007-02-01' | consumption$Date == '2007-02-02',]
-
+png("plot_1.png")
 hist(consumption$Global_active_power, xlab = 'Global Active Power', main = 'Global Active Power', col = 'red')
-png("plot1.png")
+dev.off()
